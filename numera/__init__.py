9551335c34f7f53a2625de7460a14c941c78eb6b
Amirhosein <ah.rezaie07@gmail.com>
2025-10-31 01:14:21 -0700

    Add standard_deviation func.

--- numera/__init__.py
+++ numera/__init__.py
@@ -37,3 +37,24 @@ def variance(data, sample: bool = False) -> float | int:
     avrg = mean_deviation(data)
     total = sum((avrg - number) ** 2 for number in data)
     return total / (len(data) - 1) if sample else total / len(data)
+
+    
+def standard_deviation(data, sample:bool = False) -> float | int:
+    """
+    This func calculate standard_deviation of a dataset
+
+    Args:
+        data (list or iterable of numbers): 
+            The dataset for which the standard_deviation will be calculated.
+        sample (bool, optional): 
+            Determines whether to calculate the sample standard_deviation (`True`) or 
+            the population standard_deviation (`False`). Defaults to `False`.
+
+    Returns:
+        float | int: _description_
+        
+    Example
+    >>> standard_deviation([1,2,3,4,5], sample:bool = False)
+    1.4142135623730951
+    """
+    return variance(data, sample) ** 0.5
