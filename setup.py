4d2449dbf663b8221e804867a60a191b5004b6c2
Amirhosein <ah.rezaie07@gmail.com>
2025-10-30 23:40:13 -0700

    set setup config.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="numera",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple math library",
+    author="Amirhosein Rezaie",
+    author_email="ah.rezaie07@gmail.com",
+    url="https://github.com/Amirhosein-Rezaie/numera",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6.0',
+)
