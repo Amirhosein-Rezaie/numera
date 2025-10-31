c19772fa63dbdedbf09edbf6e690d01780689955
Amirhosein <ah.rezaie07@gmail.com>
2025-10-31 00:43:52 -0700

    Add variance func.

--- numera/__init__.py
+++ numera/__init__.py
@@ -13,3 +13,27 @@ def mean_deviation(data) -> float:
     3.0
     """
     return sum(data) / len(data)
+
+
+def variance(data, sample: bool = False) -> float | int:
+    """
+    Calculates the variance of a given dataset.
+
+    Args:
+        data (list or iterable of numbers): 
+            The dataset for which the variance will be calculated.
+        sample (bool, optional): 
+            Determines whether to calculate the sample variance (`True`) or 
+            the population variance (`False`). Defaults to `False`.
+
+    Returns:
+        float:
+            The variance of the dataset.
+
+    Example:
+        >>> variance([1, 2, 3, 4, 5], sample=False)
+        2.0
+    """
+    avrg = mean_deviation(data)
+    total = sum((avrg - number) ** 2 for number in data)
+    return total / (len(data) - 1) if sample else total / len(data)
